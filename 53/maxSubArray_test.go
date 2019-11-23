0c0bbe598cc39c925165664b111c6863aae19813
yejunyi <johney_ye@foxmail.com>
2019-11-23 23:33:07 +0800

    commmit 53

---
+++ 53/maxSubArray_test.go
@@ -0,0 +1,30 @@
+package maxSubArray_test
+
+import "testing"
+
+func TestMaxSubArray(t *testing.T) {
+	t.Log(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+}
+
+/*
+ * 采用动态规划思路
+ * 每个节点最大值的关系:f(n) = max(num[n]+f(n-1), num[n])
+ */
+func maxSubArray(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+
+	max := nums[0]
+	for i, maxCurrrent := 1, max; i < l; i++ {
+		maxCurrrent += nums[i]
+		if maxCurrrent < nums[i] {
+			maxCurrrent = nums[i]
+		}
+		if max < maxCurrrent {
+			max = maxCurrrent
+		}
+	}
+	return max
+}
