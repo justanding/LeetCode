ae500bbfdbd7996604d50958fa4d59abf2b7fb55
yejunyi <johney_ye@foxmail.com>
2019-11-23 23:32:18 +0800

    add comment

--- 357/countNumbersWithUniqueDigits_test.go
+++ 357/countNumbersWithUniqueDigits_test.go
@@ -11,6 +11,15 @@ func TestCountNumbersWithUniqueDigits(t *testing.T) {
 	t.Log(countNumbersWithUniqueDigits(12))
 }
 
+/*
+ * 采用动态规划思路，
+ * 当n=0，return 1
+ * 当n=1，return 10
+ * 当n=2, return 9*9+f(1)
+ * 当n=3, return 9*(9*8)+f(2)
+ * 以此类推到n=10
+ * 当n>10，肯定有重复，等于n=10的情况
+ */
 func countNumbersWithUniqueDigits(n int) int {
 	if n == 0 {
 		return 1
