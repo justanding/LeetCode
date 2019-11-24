61d3c4af6ef95c4600a0a293aa1a3a7023347e52
yejunyi <johney_ye@foxmail.com>
2019-11-24 11:57:32 +0800

    commit 70

---
+++ 70/climbStairs_test.go
@@ -0,0 +1,27 @@
+package climbStairs_test
+
+import "testing"
+
+func TestClimbStairs(t *testing.T) {
+	t.Log(climbStairs(0))
+	t.Log(climbStairs(1))
+	t.Log(climbStairs(2))
+	t.Log(climbStairs(10))
+	t.Log(climbStairs(100))
+}
+
+/*
+*/
+func climbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	n2 := 1
+	n1 := 2
+	for i := 3; i <= n; i++ {
+		n2, n1 = n1, n2+n1
+	}
+
+	return n1
+}
